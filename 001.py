5f00f0b21d4be9aea612c824bc05d7b4734e1698
Zac <a19860101@gmail.com>
2025-05-15 20:54:14 +0800

    20250515

---
+++ 001.py
@@ -0,0 +1,20 @@
+# 變數命名原則
+# 不可使用數字或特殊符號開頭
+# 大小寫有別
+# 不可使用保留字（關鍵字
+A = 'Hello Python'
+a = '哈囉你好'
+print(A)
+print(a)
+
+# 查詢關鍵字
+import keyword
+print(keyword.kwlist)
+
+# 單行註解
+"""
+多行註解
+"""
+'''
+多行註解
+'''
---
+++ 002.py
@@ -0,0 +1,33 @@
+# 資料型態
+# int -> integer整數
+a = 123
+
+# float -> 浮點數
+b = 2.3
+
+# str -> string 字串
+c = 'Hello Python'
+
+# bool -> boolean 布林值
+d = True
+e = False
+
+# NoneType -> None
+f = None
+# print(type(f))
+
+x = '321'
+y = '123'
+# result = int(x) + int(y)
+print(x + y)
+print(int(x) + int(y))
+print(float(x) + float(y))
+print(int(x) + float(y))
+# print(int(x) + y)
+
+print(int(x) * 2)
+print(x * 2)
+
+s = 'hello '
+print(s * 5)
+
