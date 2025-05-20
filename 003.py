a59d1f2ed092663413f6641ff5ad1c842f6ccd7e
Zac <a19860101@gmail.com>
2025-05-20 19:06:56 +0800

    20250520

---
+++ 003.py
@@ -0,0 +1,21 @@
+# 運算子 operator
+
+x = 10
+y = 6
+
+# 數學
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y) #求餘數
+print(x ** y)
+print(x // y)
+
+# 比較
+print(x > y) # True
+print(x >= y) # True
+print(x < y) # False
+print(x <= y) # False
+print(x == y) # False
+print(x != y) # True
