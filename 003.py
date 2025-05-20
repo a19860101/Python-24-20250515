d4f466f22cb9449eaba1c5e0e7c2a096fc9a8ed0
Zac <a19860101@gmail.com>
2025-05-20 19:17:00 +0800

    20250520

--- 003.py
+++ 003.py
@@ -4,18 +4,42 @@ x = 10
 y = 6
 
 # 數學
-print(x + y)
-print(x - y)
-print(x * y)
-print(x / y)
-print(x % y) #求餘數
-print(x ** y)
-print(x // y)
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y) #求餘數
+# print(x ** y)
+# print(x // y)
 
 # 比較
-print(x > y) # True
-print(x >= y) # True
-print(x < y) # False
-print(x <= y) # False
-print(x == y) # False
-print(x != y) # True
+# print(x > y) # True
+# print(x >= y) # True
+# print(x < y) # False
+# print(x <= y) # False
+# print(x == y) # False
+# print(x != y) # True
+
+#指定
+x = 4
+x += 4 # x = x + 4
+print(x)
+x -= 4 # x = x - 4
+print(x)
+x *= 4 # x = x * 4
+print(x)
+x /= 4 # x = x / 4
+print(x)
+x %= 4 # x = x % 4
+print(x)
+x **= 4 # x = x ** 4
+print(x)
+x //= 4 # x = x // 4
+
+print(x)
+
+# 邏輯
+a = 10
+b = -3
+print(a > 0 and b > 0)
+print(a > 0 or b > 0)
